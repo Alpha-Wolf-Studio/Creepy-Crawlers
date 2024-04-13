eb00159c50aa361d79fb2d3f8a5a51c64f54b770
Adrian Sgro <Owinmowe@gmail.com>
2024-04-13 11:39:19 -0300

    Add possible bidirectional connection between portals

--- Assets/Scripts/Gameplay/Creatures/TeleporterCreature.cs
+++ Assets/Scripts/Gameplay/Creatures/TeleporterCreature.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Creatures
@@ -7,13 +8,28 @@ namespace Gameplay.Creatures
         [SerializeField] private TeleporterCreature linkedCreature;
         [SerializeField] private TeleporterType teleporterType;
 
+        private readonly List<Collider2D> _receivedCreatures = new List<Collider2D>();
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (teleporterType != TeleporterType.Connected) return;
 
-            if (collision.TryGetComponent(out Rigidbody2D rigidbody2D)) 
+            if (!_receivedCreatures.Contains(collision))
+                linkedCreature?.ReceiveGnome(collision);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (_receivedCreatures.Contains(collision))
+                _receivedCreatures.Remove(collision);
+        }
+
+        public void ReceiveGnome(Collider2D collision) 
+        {
+            if (collision.TryGetComponent(out Rigidbody2D rigidbody2D))
             {
-                rigidbody2D.MovePosition(linkedCreature.transform.position);
+                rigidbody2D.MovePosition(transform.position);
+                _receivedCreatures.Add(collision);
             }
         }
 
