f67c0b84fbe478d41af9d834e4055e725f6d26e4
Sauron <federicoolive@gmail.com>
2024-04-14 19:51:46 -0300

    Add level selector. Add save and load

---
+++ Assets/Scripts/Ui/Main Menu/UiControllerLevelSelector.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiControllerLevelSelector : MonoBehaviour
+{
+    public event Action<UiLevel> onLevelSelected;
+    public event Action onCloseButton;
+    public CanvasGroup canvasGroup;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private List<UiLevel> levels = new List<UiLevel>();
+
+    private void Awake()
+    {
+        UiLevel.OnLevelClicked += UiLevel_OnLevelClicked;
+        levels = transform.GetComponentsInChildren<UiLevel>().ToList();
+        closeButton.onClick.AddListener(OnCloseButton);
+    }
+
+    private void Start()
+    {
+        SaveGame save = SaveAndLoad.LoadAll();
+        bool isLocked = false;
+        int index = 0;
+
+        for (index = 0; index < save.level.Count; index++)
+        {
+            levels[index].Set(save.level[index].stars, false, "Scene" + (index + 1));
+        }
+
+        index++;
+        levels[index].Set(0, false, "Scene" + (index + 1));
+
+        for (int i = index; i < levels.Count; i++)
+        {
+            levels[i].Set(0, true, "Scene" + (i + 1));
+        }
+    }
+
+    private void OnDestroy()
+    {
+        UiLevel.OnLevelClicked -= UiLevel_OnLevelClicked;
+    }
+
+    private void OnCloseButton() => onCloseButton?.Invoke();
+
+    private void UiLevel_OnLevelClicked(UiLevel obj) => onLevelSelected?.Invoke(obj);
+}
