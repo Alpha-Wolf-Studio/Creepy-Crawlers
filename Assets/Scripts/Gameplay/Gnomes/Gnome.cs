b470fe633bf9a017bbcfb44db578a1817b65dc52
Sauron <federicoolive@gmail.com>
2024-04-14 14:29:10 -0300

    Update Gnome movement

--- Assets/Scripts/Gameplay/Gnomes/Gnome.cs
+++ Assets/Scripts/Gameplay/Gnomes/Gnome.cs
@@ -5,12 +5,16 @@ namespace Gnome
 {
     public class Gnome : MonoBehaviour
     {
-        private static readonly int IsAlive = Animator.StringToHash("IsAlive");
+        private const int MaxHeightFall = -9;
+        private static readonly int States = Animator.StringToHash("States");
 
         [SerializeField] private GnomeStats gnomeStats = new GnomeStats();
         [SerializeField] private Direction direction = Direction.Left;
-        [SerializeField] private LayerMask layerMaskCollision;
         [SerializeField] private Animator animator;
+        [SerializeField] private CollisionDetect gnomeForward;
+        [SerializeField] private CollisionDetect gnomeFloor;
+        [SerializeField] private bool isAlive = true;
+        [SerializeField] private float lastFallSpeed = 0;
 
         public bool IsFalling { get; private set; }
         public Rigidbody2D Rigidbody2D { get; private set; }
@@ -19,70 +23,27 @@ namespace Gnome
         private void Awake()
         {
             Rigidbody2D = GetComponent<Rigidbody2D>();
+            AddAllListeners();
         }
 
         private void Update()
         {
-            CheckForwardCollision();
-            CheckFallCollision();
+            CheckFall();
+        }
+
+        private void CheckFall()
+        {
+            if (lastFallSpeed > MaxHeightFall && Rigidbody2D.velocity.y < MaxHeightFall)
+            {
+                animator.GetComponent<SpriteRenderer>().color = Color.magenta; // Todo: Remover al implementar sprite y animacion
+            }
+
+            lastFallSpeed = Rigidbody2D.velocity.y;
         }
 
         private void FixedUpdate()
         {
             Move();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="direction">: -1: Left | 0: None | 1: Right</param>
-        public void Set(int direction)
-        {
-            this.direction = (Direction)direction;
-        }
-
-        public void AddExternalVelocity(Vector2 forceToAdd, ForceMode2D forceMode2D)
-        {
-            Rigidbody2D.AddForce(forceToAdd, forceMode2D);
-        }
-
-        public void Kill()
-        {
-            direction = Direction.None;
-            animator.SetBool(IsAlive, false);
-        }
-
-        private void Move()
-        {
-            float currentVel = Mathf.Abs(Rigidbody2D.velocity.x);
-            if (currentVel < gnomeStats.maxVelocity)
-            {
-                float forceToAdd = gnomeStats.forceMovement;
-                if (currentVel + forceToAdd > gnomeStats.maxVelocity)
-                {
-                    forceToAdd = gnomeStats.maxVelocity - currentVel;
-                }
-
-                Rigidbody2D.AddForce(new Vector2(forceToAdd * (int)direction, 0), ForceMode2D.Impulse);
-            }
-        }
-
-        private void CheckForwardCollision()
-        {
-            float raycastDistance = 0.1f;
-            RaycastHit2D hit =
-                    Physics2D.Raycast(transform.position, transform.right, raycastDistance, layerMaskCollision);
-
-            if (hit.collider != null)
-            {
-                if (direction == Direction.Left) direction = Direction.Right;
-                else if (direction == Direction.Right) direction = Direction.Left;
-            }
-        }
-
-        private void CheckFallCollision()
-        {
-
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -104,6 +65,93 @@ namespace Gnome
                         break;
                 }
             }
+        }
+
+        private void OnDestroy()
+        {
+            RemoveAllListeners();
+        }
+
+        private void AddAllListeners()
+        {
+            gnomeForward.onCollisionEnter += GnomeForward_onCollisionEnter;
+            gnomeFloor.onCollisionEnter += GnomeFloor_onCollisionEnter;
+            gnomeFloor.onEmptyCollisions += GnomeFloor_onEmptyCollisions;
+        }
+
+        private void RemoveAllListeners()
+        {
+            gnomeForward.onCollisionEnter -= GnomeForward_onCollisionEnter;
+            gnomeFloor.onCollisionEnter -= GnomeFloor_onCollisionEnter;
+            gnomeFloor.onEmptyCollisions -= GnomeFloor_onEmptyCollisions;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="direction">: -1: Left | 0: None | 1: Right</param>
+        public void Set(int direction)
+        {
+            this.direction = (Direction)direction;
+        }
+
+        public void AddExternalVelocity(Vector2 forceToAdd, ForceMode2D forceMode2D)
+        {
+            Rigidbody2D.AddForce(forceToAdd, forceMode2D);
+        }
+
+        public void Kill()
+        {
+            animator.GetComponent<SpriteRenderer>().color = Color.red; // Todo: Remover al implementar sprite y animacion
+            direction = Direction.None;
+            animator.SetInteger(States, (int)State.Death);
+            Rigidbody2D.velocity = Vector2.zero;
+            isAlive = false;
+        }
+
+        private void Move()
+        {
+            float currentVel = Mathf.Abs(Rigidbody2D.velocity.x);
+            if (currentVel < gnomeStats.maxVelocity)
+            {
+                float forceToAdd = gnomeStats.forceMovement;
+                if (currentVel + forceToAdd > gnomeStats.maxVelocity)
+                {
+                    forceToAdd = gnomeStats.maxVelocity - currentVel;
+                }
+
+                Rigidbody2D.AddForce(new Vector2(forceToAdd * (int)direction, 0), ForceMode2D.Impulse);
+            }
+        }
+
+        private void GnomeFloor_onCollisionEnter()
+        {
+            if (Rigidbody2D.velocity.y < MaxHeightFall)
+            {
+                Kill();
+                return;
+            }
+
+            animator.SetInteger(States, (int)State.Move);
+            animator.GetComponent<SpriteRenderer>().color = Color.white; // Todo: Remover al implementar sprite y animacion
+        }
+
+        private void GnomeFloor_onEmptyCollisions()
+        {
+            IsFalling = true;
+            animator.SetInteger(States, (int)State.Fall);
+            animator.GetComponent<SpriteRenderer>().color = Color.cyan; // Todo: Remover al implementar sprite y animacion
+        }
+
+        private void GnomeForward_onCollisionEnter()
+        {
+            transform.Rotate(Vector3.up, 180f);
+            Rigidbody2D.velocity = Vector2.zero;
+
+            if (direction == Direction.Left) 
+                direction = Direction.Right;
+            else if (direction == Direction.Right) 
+                direction = Direction.Left;
         }
     }
 
